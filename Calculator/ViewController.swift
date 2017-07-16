d01e8176348a572fb6ac5276cccbbc758a0327e8
Gavrysh <gvr@Gavryshs-MacBook-Pro.local>
2017-07-16 14:35:56 +0300

    MVP Lesson

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ievgen on 3/22/17.
+//  Copyright © 2017 Gavrysh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if digit == "." && (display.text?.range(of: ".") != nil) {
+            return
+        }
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+        print("touchDigit was called \(digit)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    var isDecimalValue: Bool {
+        get {
+            return abs(displayValue - floor(displayValue)) > 0
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+
+}
+
